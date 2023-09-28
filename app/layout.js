237ab90605eb29c641a8865088ef2dd1413163bb
Filip Lapveteläinen <filip.lapvetelainen@gmail.com>
2023-09-28 15:02:33 +0200

    moved script forward

--- app/layout.js
+++ app/layout.js
@@ -45,6 +45,46 @@ export default function RootLayout({ children }) {
         className={`${inter.variable} ${rubik.variable} ${lato.className} bg-eggshell`}
       >
         <>
+          <Script
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `  window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+            
+              if(localStorage.getItem('consentMode') === null){
+                gtag('consent', 'default', {
+                  'ad_storage': 'denied',
+                  'analytics_storage': 'denied',
+                  'functionality_storage': 'denied',
+                  'personalization_storage': 'denied',
+                  'security_storage': 'denied',
+                });
+              } else {
+                gtag('consent', 'default', JSON.parse(localStorage.getItem('consentMode')));
+              }
+            `,
+            }}
+          />
+          <Script>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=GTM-W53SWH5C`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </Script>
+          <Script
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'GTM-W53SWH5C');
+            `,
+            }}
+          />
           <GoogleAnalytics />
           <div className="absolute z-50 w-full">
             <NavContextProvider>
